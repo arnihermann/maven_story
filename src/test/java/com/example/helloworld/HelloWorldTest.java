393480748b87d2512769006bee1374418f5a8715
Árni Hermann Reynisson <arnihr@gmail.com>
2013-10-21 21:12:58 +0000

    Added a simple test.

---
+++ src/test/java/com/example/helloworld/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+    @Test
+    public void greetResultsInHello() {
+        assertEquals("Hello world", "Hello world");
+    }
+
+}
